69f37b1ad6b7ec28c9b6c82c19722a213546cc82
Sanyam gujral <70341688+gujralsanyam22@users.noreply.github.com>
2021-05-25 11:43:07 +0530

    Update test_workplace_search.py

--- tests/worplace_search/test_workplace_search.py
+++ tests/worplace_search/test_workplace_search.py
@@ -1,1 +1,287 @@
+#  Licensed to Elasticsearch B.V. under one or more contributor
+#  license agreements. See the NOTICE file distributed with
+#  this work for additional information regarding copyright
+#  ownership. Elasticsearch B.V. licenses this file to you under
+#  the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+# 	http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
 
+import datetime
+import random
+import string
+
+import pytest
+from dateutil import tz
+
+from elastic_enterprise_search import NotFoundError, UnauthorizedError, WorkplaceSearch
+
+access_token = "b6e5d30d7e5248533b5a8f5362e16853e2fc32826bc940aa32bf3ff1f1748f9b"
+content_source_id = "5f7e1407678c1d8435a949a8"
+
+CLIENT_ID = "1f87f8f6df473c06af79f88d1747afcb92e530295fad0fde340487673bec6ca6"
+CLIENT_SECRET = "d26a2c9aaa5870e8d6bdf8169aaf21ce2d66ec2e0180ffc34a0390d254135311"
+OAUTH_REDIRECT_URI = "http://localhost:8000"
+OAUTH_CODE = "7186fec34911a182606d2ab7fc36ea0ed4b8c32fef9929235cd80294422204ca"
+OAUTH_REFRESH_TOKEN = "8be8a32c22f98a28d59cdd9d2c2028c97fa6367b77a1a41cc27f2264038ee8f3"
+
+
+@pytest.fixture()
+def vcr_workplace_search():
+    yield WorkplaceSearch("http://localhost:3002", http_auth=access_token)
+
+
+@pytest.fixture()
+def workplace_search(ent_search_url):
+    with WorkplaceSearch(
+        ent_search_url, http_auth=("enterprise_search", "changeme")
+    ) as client:
+        yield client
+
+
+@pytest.fixture(scope="function")
+def content_source(workplace_search):
+    resp = workplace_search.create_content_source(
+        body={
+            "name": "Custom Content Source %s"
+            % ("".join(random.choice(string.ascii_letters) for _ in range(16)))
+        }
+    )
+    content_source_id = resp["id"]
+    yield content_source_id
+    workplace_search.delete_content_source(content_source_id=content_source_id)
+
+
+def test_content_sources(workplace_search, content_source):
+    resp = workplace_search.get_content_source(content_source)
+    assert resp.status == 200
+
+    content_source_json = resp.body.copy()
+    for field in ("name", "created_at", "last_updated_at", "groups"):
+        resp.body.pop(field)
+
+    assert resp == {
+        "id": content_source,
+        "service_type": "custom",
+        "is_remote": False,
+        "details": [],
+        "context": "organization",
+        "is_searchable": True,
+        "schema": {},
+        "display": {
+            "title_field": "external_id",
+            "subtitle_field": "url",
+            "description_field": "body",
+            "url_field": "url",
+            "detail_fields": [],
+            "color": "#000000",
+        },
+        "document_count": 0,
+        "last_indexed_at": None,
+    }
+
+    resp = workplace_search.list_content_sources()
+    assert resp.status == 200
+    assert resp == {
+        "meta": {
+            "page": {"current": 1, "total_pages": 1, "total_results": 1, "size": 25}
+        },
+        "results": [content_source_json],
+    }
+
+
+def test_documents(workplace_search, content_source):
+    dt = datetime.datetime(year=2019, month=6, day=1, hour=12, tzinfo=tz.UTC)
+    resp = workplace_search.index_documents(
+        content_source_id=content_source,
+        documents=[
+            {
+                "id": 1234,
+                "title": "The Meaning of Time",
+                "body": "Not much. It is a made up thing.",
+                "url": "https://example.com/meaning/of/time",
+                "created_at": dt,
+            },
+            {
+                "id": 1235,
+                "title": "The Meaning of Sleep",
+                "body": "Rest, recharge, and connect to the Ether.",
+                "url": "https://example.com/meaning/of/sleep",
+                "created_at": dt,
+            },
+        ],
+    )
+    assert resp.status == 200
+    assert resp == {
+        "results": [{"id": "1234", "errors": []}, {"id": "1235", "errors": []}]
+    }
+
+    resp = workplace_search.get_document(
+        content_source_id=content_source, document_id="1234"
+    )
+    assert resp.status == 200
+    resp.body.pop("last_updated")
+    assert resp == {
+        "title": "The Meaning of Time",
+        "body": "Not much. It is a made up thing.",
+        "url": "https://example.com/meaning/of/time",
+        "created_at": "2019-06-01T12:00:00Z",
+        "source": "custom",
+        "content_source_id": content_source,
+        "id": "1234",
+    }
+
+    resp = workplace_search.delete_all_documents(content_source_id=content_source)
+    assert resp.status == 200
+    assert resp == {"deleted": 0, "failures": [], "total": 0}
+
+
+def test_external_identities(workplace_search, content_source):
+    external_identity = {
+        "user": "elastic_user",
+        "source_user_id": "example@example.com",
+    }
+
+    resp = workplace_search.create_external_identity(
+        content_source_id=content_source, body=external_identity
+    )
+    assert resp.status == 200
+    assert resp == external_identity
+
+    resp = workplace_search.get_external_identity(
+        content_source_id=content_source, user="elastic_user"
+    )
+    assert resp.status == 200
+    assert resp == external_identity
+
+    resp = workplace_search.list_external_identities(content_source_id=content_source)
+    assert resp == {
+        "meta": {
+            "page": {"current": 1, "total_pages": 1, "total_results": 1, "size": 25}
+        },
+        "results": [external_identity],
+    }
+
+    resp = workplace_search.delete_external_identity(
+        content_source_id=content_source, user="elastic_user"
+    )
+    assert resp.status == 200
+    assert resp == "ok"
+
+
+def test_permissions(workplace_search, content_source):
+    resp = workplace_search.put_user_permissions(
+        content_source_id=content_source, user="elastic_user", body={"permissions": []}
+    )
+    assert resp.status == 200
+    assert resp == {"user": "elastic_user", "permissions": []}
+
+    resp = workplace_search.add_user_permissions(
+        content_source_id=content_source,
+        user="elastic_user",
+        body={"permissions": ["permission1", "permission2"]},
+    )
+    assert resp.status == 200
+    assert resp == {
+        "user": "elastic_user",
+        "permissions": ["permission1", "permission2"],
+    }
+
+    resp = workplace_search.get_user_permissions(
+        content_source_id=content_source, user="elastic_user"
+    )
+    assert resp.status == 200
+    assert resp == {
+        "user": "elastic_user",
+        "permissions": ["permission1", "permission2"],
+    }
+
+    resp = workplace_search.remove_user_permissions(
+        content_source_id=content_source,
+        user="elastic_user",
+        body={"permissions": ["permission2"]},
+    )
+    assert resp.status == 200
+    assert resp == {"user": "elastic_user", "permissions": ["permission1"]}
+
+
+@pytest.mark.vcr()
+def test_index_documents_content_source_not_found(vcr_workplace_search):
+    with pytest.raises(NotFoundError) as e:
+        vcr_workplace_search.index_documents(
+            content_source_id=content_source_id + "a",
+            documents=[
+                {
+                    "id": 1234,
+                    "title": "The Meaning of Time",
+                },
+            ],
+        )
+    assert e.value.status == 404
+    assert e.value.message == ""
+
+
+@pytest.mark.vcr()
+def test_oauth_exchange_for_access_token_code(vcr_workplace_search):
+    resp = vcr_workplace_search.oauth_exchange_for_access_token(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=OAUTH_REDIRECT_URI,
+        code="c6424958616f102ce4e8b9e776ac8547aa06c1a603a27970547648080c43abb9",
+    )
+
+    assert resp.status == 200
+    assert resp == {
+        "access_token": "00a456134c1964a0a9e82dff3ff93d8a20e9071a51f75b2bce18dde12908eb5d",
+        "expires_in": 7200,
+        "refresh_token": "57297c5f3a7fdf9dd63d03910c49c231d869e55e2e5934835c1ffa89c3c3b704",
+        "scope": "search",
+        "token_type": "Bearer",
+    }
+
+
+@pytest.mark.vcr()
+def test_oauth_exchange_for_access_token_refresh_token(vcr_workplace_search):
+    resp = vcr_workplace_search.oauth_exchange_for_access_token(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=OAUTH_REDIRECT_URI,
+        refresh_token="57297c5f3a7fdf9dd63d03910c49c231d869e55e2e5934835c1ffa89c3c3b704",
+    )
+
+    assert resp.status == 200
+    assert resp == {
+        "access_token": "494c72a1acaab2ac1dcf06882d874f5e54ce50c82d6b7183374597c2aceaddd6",
+        "expires_in": 7200,
+        "refresh_token": "43fb836d0600ce7a6e18087d4a674277493ec7be03b88756fe531b266db997f4",
+        "scope": "search",
+        "token_type": "Bearer",
+    }
+
+
+@pytest.mark.vcr()
+def test_oauth_exchange_for_access_token_invalid_grant(vcr_workplace_search):
+    # The 'code' parameter has already been used for this example
+    with pytest.raises(UnauthorizedError) as e:
+        vcr_workplace_search.oauth_exchange_for_access_token(
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+            redirect_uri=OAUTH_REDIRECT_URI,
+            code="7186fec34911a182606d2ab7fc36ea0ed4b8c32fef9929235cd80294422204ca",
+        )
+
+    assert e.value.status == 401
+    assert e.value.message == {
+        "error": "invalid_grant",
+        "error_description": "The provided authorization grant is invalid, expired, "
+        "revoked, does not match the redirection URI used in the "
+        "authorization request, or was issued to another client.",
+    }
